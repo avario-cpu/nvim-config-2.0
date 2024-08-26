1371beaddc97baefa9749f021c548df897a25a24
Loïc Villet <villet.loic@gmail.com>
2024-08-26 16:06:02 +0200

    Update grepping picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,20 +14,27 @@ return {
       local utils = require("telescope.utils")
       local icon, icon_hl = icons.get_icon(entry.filename, vim.fn.fnamemodify(entry.filename, ":e"))
       local icon_width = vim.fn.strdisplaywidth(icon or " ")
+
+      local transformed_path = utils.transform_path({ path_display = { "smart" } }, entry.filename)
+      local path_width = vim.fn.strdisplaywidth(transformed_path)
+
+      local line_col = entry.lnum .. ":" .. entry.col
+      local line_col_width = vim.fn.strdisplaywidth(line_col)
+
       local displayer = require("telescope.pickers.entry_display").create({
         separator = " ",
         items = {
           { width = icon_width },
-          { width = 40 },
-          { width = 8 },
+          { width = path_width },
+          { width = line_col_width },
           { remaining = true },
         },
       })
-      local transformed_path = utils.transform_path({ path_display = { "smart" } }, entry.filename)
+
       return displayer({
         { icon or " ", icon_hl },
         { transformed_path, "TelescopeResultsStruct" },
-        { entry.lnum .. ":" .. entry.col, "TelescopeResultsNumber" },
+        { line_col, "TelescopeResultsNumber" },
         { entry.text:gsub("^%s+", ""), "TelescopeResultsStruct" },
       })
     end
