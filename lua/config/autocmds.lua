f20cc862d05dee5878fae5dba312e98f4d88d809
Loïc Villet <villet.loic@gmail.com>
2024-08-27 22:05:23 +0200

    Center after search

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -9,3 +9,15 @@ vim.api.nvim_create_autocmd("VimEnter", {
     vim.fn.system("del /Q " .. shada_path)
   end,
 })
+
+-- Center cursor after search using Enter
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+  callback = function()
+    vim.keymap.set("c", "<CR>", function()
+      if vim.fn.getcmdtype() == "/" or vim.fn.getcmdtype() == "?" then
+        return "<CR>zz"
+      end
+      return "<CR>"
+    end, { expr = true, buffer = true })
+  end,
+})
