bb6a4341261222068e85c07ff8b7274b2d7ca2ce
Loïc Villet <villet.loic@gmail.com>
2024-08-27 17:38:01 +0200

    Change shada autocmd place

--- lua/autocmds/shada_delete.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    local shada_path = vim.fn.expand("C:\\Users\\ville\\AppData\\Local\\nvim-data\\shada\\main.shada.tmp*")
-    vim.fn.system("del /Q " .. shada_path)
-  end,
-})
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- delete dashboard weird temp shadas
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local shada_path = vim.fn.expand("C:\\Users\\ville\\AppData\\Local\\nvim-data\\shada\\main.shada.tmp*")
+    vim.fn.system("del /Q " .. shada_path)
+  end,
+})
