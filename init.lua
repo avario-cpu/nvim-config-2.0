d468f3cab71d98e6ab7758357f14f326bca5f4cd
Loïc Villet <villet.loic@gmail.com>
2024-08-27 21:45:54 +0200

    Rename lazygit custom

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("autocmds.shada_delete")
 require("scripts.insert_path")
-require("scripts.lazygit_fix")
-vim.lsp.set_log_level("debug")
+require("scripts.lazygit_custom")
+-- vim.lsp.set_log_level("debug")
