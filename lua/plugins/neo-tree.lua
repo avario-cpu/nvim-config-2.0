16db71443e3380a46d54a4b671a0003153fc5993
Loïc Villet <villet.loic@gmail.com>
2024-08-28 14:24:22 +0200

    Set neotree preview open by defaults

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,10 +3,6 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts)
       -- Ensure the filesystem key exists
-      opts.filesystem = opts.filesystem or {}
-
-      -- Ensure the window mappings exist
-      opts.window = opts.window or {}
       opts.window.mappings = opts.window.mappings or {}
 
       -- Override the "P" mapping to use float
@@ -19,6 +15,20 @@ return {
 
       opts.window.mappings["s"] = "none" -- unbind to be able to use flash
       opts.window.mappings["S"] = "open_vsplit"
+
+      -- Ensure the event_handlers key exists
+      opts.event_handlers = opts.event_handlers or {}
+      -- Add the new event handler
+      table.insert(opts.event_handlers, {
+        event = "after_render",
+        handler = function()
+          local state = require("neo-tree.sources.manager").get_state("filesystem")
+          if not require("neo-tree.sources.common.preview").is_active() then
+            state.config = { use_float = true } -- or whatever your config is
+            state.commands.toggle_preview(state)
+          end
+        end,
+      })
 
       return opts
     end,
