b9178b874c5175a2946f967994d89523eef8b0df
Loïc Villet <villet.loic@gmail.com>
2024-09-01 01:04:46 +0200

    Turn off minipair in cmd

---
+++ lua/plugins/mini.pairs.lua
@@ -0,0 +1,12 @@
+return {
+  "echasnovski/mini.pairs",
+  opts = function(_, opts)
+    -- Your other mini.pairs settings here
+    opts.modes = {
+      insert = true,
+      command = false,
+      terminal = false,
+    }
+  end,
+  enalbed = true,
+}
