ee7dcba551c4751571c0bf25345212d6054e18f6
Loïc Villet <villet.loic@gmail.com>
2024-09-01 02:05:51 +0200

    Add superTab for copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -10,7 +10,7 @@ return {
       hide_during_completion = true,
       debounce = 50,
       keymap = {
-        accept = "<Tab>",
+        accept = false,
         accept_word = "<M-l>",
         accept_line = "<M-;>",
         next = "<M-]>",
@@ -18,6 +18,26 @@ return {
         dismiss = "<C-]>",
       },
     }
+    opts.copilot_node_command = "node" -- Node.js version must be > 16.x
+    opts.server_opts_overrides = {
+      settings = {
+        advanced = {
+          listCount = 10, -- #completions for panel
+          inlineSuggestCount = 3, -- #completions for getCompletions
+        },
+      },
+    }
+
+    -- Keep tab default behavior in insert mode
+    vim.keymap.set("i", "<Tab>", function()
+      if require("copilot.suggestion").is_visible() then
+        require("copilot.suggestion").accept()
+      else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+      end
+    end, {
+      silent = true,
+    })
     return opts
   end,
 }
