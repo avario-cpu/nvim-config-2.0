fc02474c32d35af35d3995be981fc6a017c70f5f
Loïc Villet <villet.loic@gmail.com>
2024-09-01 13:51:52 +0200

    Toggle coc

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -1,6 +1,7 @@
 return {
   "neoclide/coc.nvim",
   branch = "release",
+  enabled = false,
   vim.api.nvim_set_keymap("n", "gR", "<Plug>(coc-references)", { silent = true, noremap = true }),
   vim.keymap.set("v", "<leader>cR", "<Plug>(coc-codeaction-selected)"),
 }
