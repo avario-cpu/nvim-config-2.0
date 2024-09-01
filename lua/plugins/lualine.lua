3c5dac3fda9b3232a629a50baae4a27cbf3e6586
Loïc Villet <villet.loic@gmail.com>
2024-09-01 02:05:29 +0200

    cleanup lualine changes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,20 +6,7 @@ return {
     -- Override specific highlights after the theme is set
     local custom_horizon = require("lualine.themes.horizon")
     -- local custom_bg = "#232336"
-
-    -- Set pink background for all available modes
     -- custom_horizon.normal.c.bg = custom_bg
-    -- custom_horizon.insert.c.bg = custom_bg
-    -- custom_horizon.visual.c.bg = custom_bg
-    -- custom_horizon.replace.c.bg = custom_bg
-    -- custom_horizon.command.c.bg = custom_bg
-    -- custom_horizon.inactive.c.bg = custom_bg
-    -- if custom_horizon.terminal then
-    --   custom_horizon.terminal.c.bg = custom_bg
-    -- end
-    -- if custom_horizon.select then
-    --   custom_horizon.select.c.bg = custom_bg
-    -- end
 
     opts.options.theme = custom_horizon
 
