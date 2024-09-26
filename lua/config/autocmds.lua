dcb3316beaf1e14dd13258ecc0e8c515910220f3
Loïc Villet <villet.loic@gmail.com>
2024-09-26 23:31:35 +0200

    Set textlength option for python files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- remove automatic comment insertion
 vim.cmd([[autocmd FileType * set formatoptions-=ro]])
 
 -- Center cursor after search using Enter
@@ -30,11 +31,20 @@ vim.api.nvim_create_autocmd("FileType", {
   command = "setlocal wrap linebreak",
 })
 
+-- automatically switch to the help window when opening help files to the right
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = { "*.txt", "*.md" },
   callback = function()
     if vim.bo.buftype == "help" then
       vim.cmd("wincmd L")
     end
+  end,
+})
+
+-- Set textwidth to 88 for Python files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.bo.textwidth = 88 -- Set textwidth to 88 for Python files (matching Black's default)
   end,
 })
