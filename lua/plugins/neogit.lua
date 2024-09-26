e60e88d0c0cbf7c47bdd995b1c7f5d133b994571
Loïc Villet <villet.loic@gmail.com>
2024-09-27 00:13:47 +0200

    Plugin toggles

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,8 +1,8 @@
 return {
   "NeogitOrg/neogit",
+  enabled = false,
   dependencies = {
     "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
 
     -- Only one of these is needed.
     "nvim-telescope/telescope.nvim", -- optional
