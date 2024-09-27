0c0db2bd00c4e434e03fc30294d7cab6cf0f197a
Loïc Villet <villet.loic@gmail.com>
2024-09-27 14:41:19 +0200

    Wip: Add function to grep for python imports

--- lua/scripts/search_python_imports.lua
+++ lua/scripts/search_python_imports.lua
@@ -1,29 +1,27 @@
--- Yank relative path to clipboard
+-- Yank buffer's relative path to clipboard
 vim.keymap.set("n", "<leader>yp", function()
   local relative_path = vim.fn.expand("%:p:~:.")
   vim.fn.setreg("+", relative_path)
   vim.notify("Relative path copied to clipboard", vim.log.levels.INFO)
 end, { noremap = true, silent = true, desc = "Yank relative path to clipboard" })
 
+local telescope = require("telescope.builtin")
+
 -- Search for imports of the current file, piped to quickfix list
-vim.keymap.set("n", "<leader>si", function()
-  local path = vim.fn.expand("%:p:~:."):gsub("%.[^.]+$", ""):gsub("\\", "/")
-  local import_path = path:gsub("/", [[\.]])
+function Find_current_buffer_imports()
+  local current_file = vim.fn.expand("%:t")
+  local module_name = current_file:gsub("%.py$", "")
 
-  -- Create patterns for both import styles
-  local patterns = {
-    string.format([[from %s import]], import_path),
-    string.format([[import %s]], import_path),
-  }
+  if module_name ~= "" then
+    telescope.grep_string({
+      prompt_title = "Imports of " .. module_name,
+      search = module_name .. ".*import",
+      use_regex = true,
+    })
+  else
+    print("Not a valid Python file")
+  end
+end
 
-  -- Combine patterns into a single grep command
-  local grep_pattern = table.concat(patterns, "\\|")
-  local cmd = string.format('silent grep! "%s"', grep_pattern)
-
-  vim.notify("Searching for imports: " .. cmd, vim.log.levels.INFO)
-
-  -- Execute the grep command and open the quickfix window without jumping
-  vim.cmd(cmd)
-  vim.cmd("copen")
-  vim.cmd("wincmd p") -- Move cursor back to the previous window
-end, { noremap = true, silent = true, desc = "Search for imports of current file" })
+vim.api.nvim_create_user_command("FindCurrentBufferImports", Find_current_buffer_imports, {})
+vim.api.nvim_set_keymap("n", "<leader>si", ":FindCurrentBufferImports<CR>", { noremap = true, silent = true })
